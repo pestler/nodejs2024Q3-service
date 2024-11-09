666723b630f1215841017e7a8feb02af5a605529
Aleh Kuis <pestleralex@gmail.com>
2024-11-09 15:10:50 +0300

    feat: add module cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumModule } from './src/album/album.module';
+import { UsersModule } from './src/users/users.module';
+import { TracksModule } from './src/tracks/tracks.module';
+import { ArtistsModule } from './src/artists/artists.module';
+import { FavoritesModule } from './src/favorites/favorites.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AlbumModule,
+    UsersModule,
+    TracksModule,
+    ArtistsModule,
+    FavoritesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
