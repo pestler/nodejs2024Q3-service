d5f54980699506c67b4aed2574c537650dbefce3
Aleh Kuis <pestleralex@gmail.com>
2024-11-12 17:31:48 +0300

    feat: add album service

--- src/albums/albums.service.ts
+++ src/albums/albums.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { dataBase } from './../database/database';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
+import { Album } from 'src/database/interface';
+import { v4 as uuid } from 'uuid';
+import { StatusCodes } from 'http-status-codes';
 
 @Injectable()
 export class AlbumsService {
-  create(createAlbumDto: CreateAlbumDto) {
-    return 'This action adds a new album';
+  create(createAlbumDto: CreateAlbumDto): Album[] {
+    const album = {
+      id: uuid(),
+      ...createAlbumDto,
+    };
+    dataBase.albums.push(album);
+    return dataBase.albums;
   }
 
   findAll() {
-    return `This action returns all albums`;
+    return dataBase.albums;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} album`;
+  findOne(id: string) {
+    return dataBase.albums.find((album) => album.id === id);
   }
 
-  update(id: number, updateAlbumDto: UpdateAlbumDto) {
-    return `This action updates a #${id} album`;
+  update(id: string, updateAlbumDto: UpdateAlbumDto) {
+    const album = dataBase.albums.find((album) => album.id == id);
+    if (!album)
+      throw new HttpException("album doesn't exists", StatusCodes.NOT_FOUND);
+    album.name = updateAlbumDto.name;
+    album.year = updateAlbumDto.year;
+    album.artistId = updateAlbumDto.artistId;
+    return album;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} album`;
+  remove(id: string) {
+    const indexAlbum = dataBase.albums.findIndex((album) => album.id == id);
+    if (indexAlbum == -1)
+      throw new HttpException("album doesn't exists", StatusCodes.NOT_FOUND);
+    dataBase.albums.splice(indexAlbum, 1);
   }
 }
--- src/albums/dto/create-album.dto.ts
+++ src/albums/dto/create-album.dto.ts
@@ -1,1 +1,15 @@
-export class CreateAlbumDto {}
+import { IsNotEmpty, IsNumber, IsString, ValidateIf } from 'class-validator';
+
+export class CreateAlbumDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  year: number;
+
+  @IsString()
+  @ValidateIf((_, value) => value !== null)
+  artistId: string;
+}
